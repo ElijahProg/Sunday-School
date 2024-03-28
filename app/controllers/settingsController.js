a6c1858f01b566cd722367af64b090a3393b6a4c
Elias Yalew <eliasyalew1997@gmail.com>
2024-03-28 17:02:22 +0300

    add fixes

--- app/controllers/settingsController.js
+++ app/controllers/settingsController.js
@@ -4,7 +4,6 @@ const Subject = require("../models/view_model/subjectViewModel");
 exports.gradesList = async function (req, res) {
   try {
     var grades = await Grade.find({});
-    console.log(grades);
     res.render("grade-list", { grades: grades });
   } catch (ex) {
     console.log(`Exception-->${ex}`);
@@ -25,20 +24,32 @@ exports.addGrade = async function (req, res) {
       ],
       async function (response) {
         const gradeData = req.body;
-        if (response.success) {
-          var newGrade = new Grade({
+        if (gradeData.id) {
+          var patchData = {
             name: gradeData.name,
             level: gradeData.level,
             minAge: gradeData.minAge,
-            maxAge: gradeData.maxAge,
-            createBy: currentUser._id,
-          });
-          await newGrade.save();
-          console.log(`Grades Created`);
+            maxAge: gradeData.maxAge
+          }
+          await Grade.findByIdAndUpdate(gradeData.id, patchData);
           res.redirect("/setting/grade-list");
+
         } else {
-          console.log(response);
+          if (response.success) {
+            var newGrade = new Grade({
+              name: gradeData.name,
+              level: gradeData.level,
+              minAge: gradeData.minAge,
+              maxAge: gradeData.maxAge,
+              createBy: currentUser._id,
+            });
+            await newGrade.save();
+            res.redirect("/setting/grade-list");
+          } else {
+            console.log(response);
+          }
         }
+
       }
     );
   } catch (ex) {
@@ -47,28 +58,15 @@ exports.addGrade = async function (req, res) {
   }
 };
 
-exports.updateGrade = async function (req, res) {
+exports.removeGrade = async function (req, res) {
   try {
-    utils.check_request_params(
-      req.body,
-      [
-        { name: "id", type: "string" },
-        { name: "name", type: "string" },
-        { name: "level", type: "string" },
-        { name: "minAge", type: "string" },
-        { name: "maxAge", type: "string" },
-      ],
-      async function (response) {
-        if (response.success) {
-        } else {
-        }
-      }
-    );
+    var grade = await Grade.findByIdAndDelete(req.body.id);
+    res.redirect("/setting/grade-list");
   } catch (ex) {
-    console.log(`Exception->updateGrade ${ex}`);
-    res.render("error-page", { exception: ex });
+    console.log(`Exception->Remove Grade${ex}`)
+    res.render("error-page", { exception: ex })
   }
-};
+}
 
 exports.subjectsList = async function (req, res) {
   try {
@@ -78,7 +76,6 @@ exports.subjectsList = async function (req, res) {
       async function (response) {
         if (response.success) {
           var grade = await Grade.findById(req.query.id);
-          console.log(grade);
           res.render("subject-list", { grade: grade });
         } else {
           console.log(`request parameter validation issue${response}`);
@@ -104,16 +101,42 @@ exports.addSubject = async function (req, res) {
       ],
       async function (response) {
         if (response.success) {
-          var subject = new Subject({
-            name: req.body.name,
-            semester: req.body.semester,
-            createBy: currentUser._id,
-          });
-          await Grade.findOneAndUpdate(
-            { _id: req.body.id },
-            { $push: { subjects: subject } }
-          );
-          res.redirect("/setting/subject-list?id=" + req.body.id);
+          if (req.body.subId) {
+            var listToUpdate = await Grade.find({
+              _id: req.body.grade
+            })
+            console.log(listToUpdate)
+            const updateDocument = {
+              "subjects.$[i].name": req.body.name,
+              "subjects.$[i].semester": req.body.semester
+            };
+
+            const options = {
+              arrayFilters: [
+                {
+                  "i._id": { $eq: req.body.subId },
+                }
+              ]
+            };
+            var result = await Grade.findOneAndUpdate(
+              {
+                _id: req.body.grade
+              },updateDocument,options)
+            console.log(result)
+            res.redirect("/setting/subject-list?id=" + req.body.id);
+          } else {
+            var subject = new Subject({
+              name: req.body.name,
+              semester: req.body.semester,
+              createBy: currentUser._id,
+            });
+            await Grade.findOneAndUpdate(
+              { _id: req.body.id },
+              { $push: { subjects: subject } }
+            );
+            res.redirect("/setting/subject-list?id=" + req.body.id);
+          }
+
         } else {
           console.log(`request parameter validation issue${response}`);
         }
--- app/routes/setting.js
+++ app/routes/setting.js
@@ -5,6 +5,7 @@ const { protectRoute } = require("../auth/protect");
 
 router.get("/setting/grade-list",protectRoute, setting.gradesList);
 router.post("/setting/grade-add",protectRoute, setting.addGrade);
+router.post("/setting/grade-remove",protectRoute, setting.removeGrade);
 router.get("/setting/subject-list",protectRoute, setting.subjectsList);
 router.post("/setting/subject-add",protectRoute, setting.addSubject);
 module.exports = router;
